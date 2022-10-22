cc49a5e123456552265eeb28d67f77ed0cb027e9
SeungYongShim <syshim@me.com>
2022-10-22 21:28:44 +0900

    .

--- src/ForwardAlgebraic.Effects.Http.Tests/UnitTest1.cs
+++ src/ForwardAlgebraic.Effects.Http.Tests/UnitTest1.cs
@@ -2,8 +2,12 @@ using System.Net.Http.Json;
 using System.Text.Json;
 using ForwardAlgebraic.Effects.Abstractions;
 using LanguageExt;
+using LanguageExt.Pipes;
 using Microsoft.AspNetCore;
 using Microsoft.Extensions.Hosting;
+using WireMock.RequestBuilders;
+using WireMock.ResponseBuilders;
+using WireMock.Server;
 
 namespace ForwardAlgebraic.Effects.Http.Tests;
 
@@ -11,13 +15,45 @@ namespace ForwardAlgebraic.Effects.Http.Tests;
 
 public class UnitTest1
 {
-    record TestResponse(JsonElement Json);
+    record TestResponse(string Hello);
+
     [Fact]
     public async Task Test1()
     {
         var httpClient = new HttpClient
         {
             BaseAddress = new Uri("https://postman-echo.com/")
+        };
+
+        var q = Http<RT>.PostAff<dynamic>("post", new
+        {
+            Hello = "World"
+        });
+
+        using var cts = new CancellationTokenSource();
+        var r = await q.Run(new RT(httpClient, cts));
+
+        var x = r.ThrowIfFail();
+    }
+
+    [Fact]
+    public async Task Test2()
+    {
+        using var server = WireMockServer.Start();
+
+        server.Given(Request.Create().WithPath("/post").UsingPost())
+              .RespondWith(Response.Create()
+                                   .WithStatusCode(200)
+                                   .WithHeader("Content-Type", "text/plain")
+                                   .WithBodyAsJson(new
+                                   {
+                                       Hello = "World"
+                                   }));
+
+        
+        var httpClient = new HttpClient
+        {
+            BaseAddress = new Uri(server.Url)
         };
 
         var q = Http<RT>.PostAff<TestResponse>("post", new
@@ -28,7 +64,7 @@ public class UnitTest1
         using var cts = new CancellationTokenSource();
         var r = await q.Run(new RT(httpClient, cts));
 
-        var x = r.ThrowIfFail();
+        Assert.True(r.ThrowIfFail() is { Hello: "World" }) ;
     }
 
     public readonly record struct RT(HttpClient It, CancellationTokenSource CancellationTokenSource) :
